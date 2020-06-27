44febe9548a913487df1da6449492315cd655869
Manish Pandey <2018140@iiitdmj.ac.in>
2020-06-27 09:32:14 +0530

    Third Commit

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,21 +1,191 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class DashBoard extends StatelessWidget {
+class DashBoard extends StatefulWidget {
+  @override
+  _DashBoardState createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
+    var height = 0.0;
     Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Color(0xFF09144B),
         body: Column(
           children: [
             Container(
-              margin: EdgeInsets.only(right: 10),
-              height: size.height * 0.2,
+              height: size.height * 0.22,
               width: size.width,
-              decoration: BoxDecoration(
-                  color: Color(0xFF09144B),
-                  borderRadius:
-                      BorderRadius.only(bottomRight: Radius.circular(50))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Text(
+                          'q',
+                          style: TextStyle(
+                            color: Color(0xFFFF397F),
+                            fontFamily: 'MuseoModerno',
+                            fontSize: 35,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          'Hey',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          'Manish'.toUpperCase(),
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w100),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          'Level',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          '2'.toUpperCase(),
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: size.height * 0.67,
+                width: size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(80))),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15, right: 10),
+                          child: Text(
+                            'Level Up',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50, right: 10),
+                      child: SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          thumbColor: Color(0xFFEB1555),
+                          activeTrackColor: Colors.white,
+                          inactiveTrackColor: Color(0xFF09144B),
+                          overlayColor: Color(0x29EB1555),
+                          thumbShape:
+                              RoundSliderThumbShape(enabledThumbRadius: 13.0),
+                          overlayShape:
+                              RoundSliderOverlayShape(overlayRadius: 20.0),
+                        ),
+                        child: Slider(
+                          value: height,
+                          min: 0.0,
+                          max: 100.0,
+                          onChanged: (double newValue) {
+                            setState(() {
+                              height = newValue;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 60),
+                          child: Text(
+                            '0',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Text(
+                            '100',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
