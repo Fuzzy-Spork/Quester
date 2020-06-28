043986bff2315f08d529e985e140ade1880708ee
Sehej <sehejjain@outlook.com>
2020-06-28 19:21:56 +0530

    Added Completion Report

--- lib/models/mission/mission.g.dart
+++ lib/models/mission/mission.g.dart
@@ -8,20 +8,27 @@ part of 'mission.dart';
 
 Mission _$MissionFromJson(Map<String, dynamic> json) {
   return Mission(
+    id: json['id'] as String,
     desc: json['desc'] as String,
     name: json['name'] as String,
     xp: json['xp'] as int,
     category: _$enumDecodeNullable(_$CategoryEnumMap, json['category']),
     status: _$enumDecodeNullable(_$StatusEnumMap, json['status']),
+    completionReport: json['completionReport'] == null
+        ? null
+        : CompletionReport.fromJson(
+            json['completionReport'] as Map<String, dynamic>),
   );
 }
 
 Map<String, dynamic> _$MissionToJson(Mission instance) => <String, dynamic>{
+      'id': instance.id,
       'name': instance.name,
       'desc': instance.desc,
       'xp': instance.xp,
       'category': _$CategoryEnumMap[instance.category],
       'status': _$StatusEnumMap[instance.status],
+      'completionReport': instance.completionReport?.toJson(),
     };
 
 T _$enumDecode<T>(
--- lib/models/mission/missions.dart
+++ lib/models/mission/missions.dart
@@ -1,3 +1,5 @@
+import 'package:quester/models/completion_report/completion_report.dart';
+
 import 'mission.dart';
 
 List<Mission> missions = [
@@ -9,6 +11,7 @@ List<Mission> missions = [
         'If each of us plants a tree, there will be enough trees to keep Earth in shape for years to come! Show Earth some love by giving new life.',
     xp: 50,
     status: Status.Inactive,
+    completionReport: CompletionReport(),
   ),
   Mission(
     id: 'startGarden',
@@ -18,6 +21,7 @@ List<Mission> missions = [
         'Gardens needn\'t be huge, start small, get a potted plant. Start Somewhere!',
     xp: 40,
     status: Status.Inactive,
+    completionReport: CompletionReport(),
   ),
   Mission(
     id: 'waterPlantsRepeatable',
@@ -26,6 +30,7 @@ List<Mission> missions = [
     desc: 'Have a garden? A potted plant? Go water\'em all!',
     xp: 10,
     status: Status.Inactive,
+    completionReport: CompletionReport(),
   ),
   Mission(
     id: 'donate',
@@ -34,6 +39,7 @@ List<Mission> missions = [
     desc: 'Every bit matter! Help where tou can!',
     xp: 100,
     status: Status.Inactive,
+    completionReport: CompletionReport(),
   ),
   Mission(
     id: 'adopt',
@@ -42,26 +48,27 @@ List<Mission> missions = [
     desc: 'Give someone a new home!',
     xp: 100,
     status: Status.Inactive,
+    completionReport: CompletionReport(),
   ),
   // Mission(
   //   category: Category.Repeatable,
   //   name: '',
   //   desc: '',
   //   xp: 50,
-  //   status: Status.Inactive,
+  //   status: Status.Inactive,completionReport: CompletionReport(),
   // ),
   // Mission(
   //   category: Category.Repeatable,
   //   name: '',
   //   desc: '',
   //   xp: 50,
-  //   status: Status.Inactive,
+  //   status: Status.Inactive,completionReport: CompletionReport(),
   // ),
   // Mission(
   //   category: Category.Repeatable,
   //   name: '',
   //   desc: '',
   //   xp: 50,
-  //   status: Status.Inactive,
+  //   status: Status.Inactive,completionReport: CompletionReport(),
   // ),
 ];
