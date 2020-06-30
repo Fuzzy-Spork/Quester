cf289b630a65036c9505b49810303eb7d5110753
Sehej Jain <Sehejjain@outlook.com>
2020-06-30 15:07:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Quester',
       home: LandingPage(),
     );
   }
